c86af3836b2d872cba7af10aec8760bc20b8b38b
SeongGwangJu <seonggwangdev@gmail.com>
2023-10-25 15:51:37 +0900

    FE : OAuth2 계정 통합

--- board_front/src/Routes/AuthRoute.jsx
+++ board_front/src/Routes/AuthRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import Signin from '../pages/Signin/Signin';
 import Signup from '../pages/Signup/Signup';
 import { useQueryClient } from 'react-query';
+import SignupOauth2Merge from '../pages/Signup/SignupOauth2Merge';
 
 function AuthRoute(props) {
 
@@ -19,6 +20,7 @@ function AuthRoute(props) {
             <Route path="signin" element={<Signin />} />
             <Route path="signup" element={<Signup />} />
             <Route path="oauth2/signup" element={ <SignupOauth2 /> } />
+            <Route path="oauth2/signup/merge" element={ <SignupOauth2Merge /> } />
         </Routes>
     );
 }
